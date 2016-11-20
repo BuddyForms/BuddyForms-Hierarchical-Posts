e24db5cabe57a0337962c22ec35710d85e982955
Sven Lehnert <mail@sven-lehnert.de>
2016-11-20 20:53:00 +0100

    add dependencies management with tgm

--- loader.php
+++ loader.php
@@ -29,16 +29,45 @@
  */
 
 
-add_action( 'plugins_loaded', 'buddyforms_hierarchical_requirements' );
-function buddyforms_hierarchical_requirements() {
+//
+// Check the plugin dependencies
+//
+add_action('init', function(){
 
-	if ( ! defined( 'BUDDYFORMS_VERSION' ) ) {
-		add_action( 'admin_notices', create_function( '', 'printf(\'<div id="message" class="error"><p><strong>\' . __(\'BuddyForms Hierarchical Form Elements needs BuddyForms to be installed. <a target="_blank" href="%s">--> Get it now</a>!\', " wc4bp_xprofile" ) . \'</strong></p></div>\', "http://themekraft.com/store/wordpress-front-end-editor-and-form-builder-buddyforms/" );' ) );
-
+	// Only Check for requirements in the admin
+	if(!is_admin()){
 		return;
 	}
 
-}
+	// Require TGM
+	require ( dirname(__FILE__) . '/includes/resources/tgm/class-tgm-plugin-activation.php' );
+
+	// Hook required plugins function to the tgmpa_register action
+	add_action( 'tgmpa_register', function(){
+
+		// Create the required plugins array
+		$plugins = array(
+			array(
+				'name'              => 'BuddyForms',
+				'slug'              => 'buddyforms',
+				'required'          => true,
+			),
+		);
+
+		$config = array(
+			'id'           => 'buddyforms-hp',         // Unique ID for hashing notices for multiple instances of TGMPA.
+			'parent_slug'  => 'edit.php?post_type=buddyforms',            // Parent menu slug.
+			'capability'   => 'manage_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+			'has_notices'  => true,                    // Show admin notices or not.
+			'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+			'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+		);
+
+		// Call the tgmpa function to register the required plugins
+		tgmpa( $plugins, $config );
+
+	} );
+}, 1, 1);
 
 add_action( 'init', 'buddyforms_hierarchical_require' );
 function buddyforms_hierarchical_require() {
@@ -46,7 +75,6 @@ function buddyforms_hierarchical_require() {
 	require( dirname( __FILE__ ) . '/includes/functions.php' );
 	require( dirname( __FILE__ ) . '/includes/form-elements.php' );
 }
-
 
 add_action( 'buddyforms_front_js_css_enqueue', 'buddyforms_hierarchical_front_js_css_enqueue' );
 function buddyforms_hierarchical_front_js_css_enqueue() {
