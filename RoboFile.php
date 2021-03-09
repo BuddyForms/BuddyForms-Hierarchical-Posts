fbb6dc02c561cbbf3af9bb414e7b6ff89c72faa2
jnfDev <jairo.jnf.2010@gmail.com>
2021-03-09 10:25:17 -0400

    Correction on the Robo file.

--- RoboFile.php
+++ RoboFile.php
@@ -24,7 +24,7 @@ class RoboFile extends RoboFileBase {
 	}
 
 	public function minifyAssetsDirectories() {
-		return array( 'assets' );
+		return array();
 	}
 
 	public function minifyImagesDirectories() {
